99c17b21aea755ab562f61df4865d463afbee19c
JaeheeLee-be <clodusky1@gmail.com>
2026-02-06 18:00:18 +0900

    practice

--- Day1/practice_3/app.py
+++ Day1/practice_3/app.py
@@ -1,18 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/") # 127.0.0.1:5000
-def home():
-    return "홈입니다"
-
-@app.route("/hello") # 127.0.0.1:5000/hello
+@app.route("/hello")
 def hello():
-    return "안녕하세요"
-
-@app.route("/user/<name>")
-def greet(name):
-    return f"{name}님 환영합니다."
+    return render_template("hello.html", name="OZ")
 
 if __name__ == "__main__":
     app.run(debug=True)
