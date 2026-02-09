f7d61ef90d6cb674bd6ed0dde91938ec37706ef3
JaeheeLee-be <clodusky1@gmail.com>
2026-02-09 15:09:20 +0900

    practice

--- Flask/Day4/practice_1/app.py
+++ Flask/Day4/practice_1/app.py
@@ -1,4 +1,19 @@
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import declarative_base, sessionmaker
+from flask import Flask
+from flask_sock import Sock
 
-# 데이터 베이스 연결
+
+app = Flask(__name__)
+sock = Sock(app)
+
+@sock.route("/ws")
+def websocket(ws):
+    while True:
+        data = ws.receive() # postman에서 서버한테 주는 메시지
+
+        if data is None:
+            break
+
+        ws.send(f"서버에서 보내는 메시지: {data}") # 서버가 postman에게 보내는 메시지
+
+if __name__ == "__main__":
+    app.run(debug=True)
